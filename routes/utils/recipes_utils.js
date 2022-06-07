2641436b77ce0621569cefab151f03f619a65edf
HadarSabati <hadarsabati@gmail.com>
2022-06-07 17:14:48 +0300

    recipe utils update

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -38,8 +38,63 @@ async function getRecipeDetails(recipe_id) {
 }
 
 
+//async function getRecipesPreview
+
+async function exractPreviewRecipeDetails(recipes_info){
+    //return (recipes_info.length)
+    return recipes_info.map((recipe_info) => {
+        //check the data type so it can work with diffrent types of data
+        let data = recipe_info;
+        if (recipe_info.data) {
+            data = recipe_info.data;
+        }
+        const {
+            id,
+            title,
+            readyInMinutes,
+            image,
+            aggregateLikes,
+            vegan,
+            vegetarian,
+            glutenFree,
+        } = data;
+        return {
+            id: id,
+            title: title,
+            image: image,
+            readyInMinutes: readyInMinutes,
+            popularity: aggregateLikes,
+            vegan: vegan,
+            vegetarian: vegetarian,
+            glutenFree: glutenFree
+        }
+    })
+}
+
+
+async function getRandomRecipes(){
+    const response = await axios.get(`${api_domain}/random`, {
+        params:{
+            number: 10,
+            apiKey: process.env.spooncular_apiKey
+        }
+    })
+    return response;
+}
+async function getRandomQuantityRecipes(quantity){
+    let random_pool = await getRandomRecipes();
+    let filtered_random_recipes = random_pool.data.recipes.filter((random) => (random.instructions != "") && (random.image)) //TODO: figure whats missing here
+    if(filtered_random_recipes.length < 3){
+        return getRandomQuantityRecipes(quantity)
+    }
+    let new_filtered_random_recipes = filtered_random_recipes.slice(0, quantity)
+    return exractPreviewRecipeDetails(new_filtered_random_recipes)
+}
+
+
 
 exports.getRecipeDetails = getRecipeDetails;
+exports.getRandomQuantityRecipes = getRandomQuantityRecipes;
 
 
 
